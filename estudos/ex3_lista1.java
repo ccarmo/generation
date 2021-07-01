15c2e0fa72f6b808d7723c51cc976cd44ccdec07
Carlos Carmo <carlosh.carmo@hotmail.com>
2021-07-01 20:25:37 -0300

    Update ex3_lista1.java

--- estudos/ex3_lista1.java
+++ estudos/ex3_lista1.java
@@ -4,27 +4,27 @@ import java.util.Scanner;
 
 public class mostrarHorarios {
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 
-    float segundos;
-	  double minutos,horas;
+     float segundos;
+     double minutos,horas;
+      
+     Scanner leia = new Scanner(System.in);
 
-	  Scanner leia = new Scanner(System.in);
+     System.out.println("\nInsira o tempo do evento (em segundos): ");
+     segundos = leia.nextFloat();
 
-	  System.out.println("\nInsira o tempo do evento (em segundos): ");
-	  segundos = leia.nextFloat();
+     //calculos do tempo inserido (segundos) em horas e minutos
 
-	  //calculos do tempo inserido (segundos) em horas e minutos
+     //conversão para horas
+     horas = segundos*(0.000277778);
 
-    //conversão para horas
-    horas = segundos*(0.000277778);
-
-    //conversão para minutos
-    minutos = segundos*0.0166667;
+     //conversão para minutos
+     minutos = segundos*0.0166667;
 
 
-	  System.out.printf("O tempo do evento em horas, minutos e segundos é: %.2f horas : %.2f minutos : %.2f segundos",horas,minutos,segundos);
-
-	}
+     System.out.printf("O tempo do evento em horas, minutos e segundos é: %.2f horas : %.2f minutos : %.2f segundos",horas,minutos,segundos);
+  
+  }
 
 }
