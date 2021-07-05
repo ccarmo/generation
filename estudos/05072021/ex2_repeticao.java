a8f5b9de730d78e0fc548570b21fe6f50c773bb7
Carlos Carmo <carlosh.carmo@hotmail.com>
2021-07-05 15:46:13 -0300

    Update ex2_repeticao.java

--- estudos/05072021/ex2_repeticao.java
+++ estudos/05072021/ex2_repeticao.java
@@ -5,19 +5,22 @@ import java.util.Scanner;
 public class imprimeParImpar {
     
 
-	public static void main(String[] args) {
+  public static void main(String[] args) 
+  {
 	  
-	  Scanner leia = new Scanner(System.in);
-	  float numero;
+      Scanner leia = new Scanner(System.in);
+      float numero;
 	  
       for(int contador = 1;contador<=10;contador++) {
     	  System.out.println("\nDigite um número:");
     	  numero = leia.nextFloat();  
-    	  if(numero%2 == 0) {
+    	  if(numero%2 == 0) 
+	  {
     		System.out.println("O "+numero+" é par");
-    	  } else {
+    	  } else 
+	  {
     		System.out.println("O "+numero+" é impar");
     	  }
-     }  		  	  
-  }
+       }  		  	  
+   }
 }
