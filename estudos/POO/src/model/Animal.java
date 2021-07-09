98f6fa554e0888383af175327119aa6d0296de5a
Carlos Henrique <carlosh.carmo@hotmail.com>
2021-07-08 22:14:24 -0300

    Adicionado getters and setters Animal

--- estudos/POO/src/model/Animal.java
+++ estudos/POO/src/model/Animal.java
@@ -5,16 +5,56 @@ public class Animal {
 	
 	private String nome;
 	private int    idade;
+	private float tamanho;
+	private float peso;
+	private boolean xixi;
 	
 	
-	public Animal (String nome, int idade){
+	public Animal (String nome, int idade, float comprimento, float peso){
           super();
           this.nome = nome;
-          this.idade = idade;		  
+          this.idade = idade;
+          this.comprimento = comprimento;
+          this.peso = peso;	
+          	  
+	}
+	
+	public void fazerXixi(boolean xixi){
+		if(xixi == true)
+		{
+          System.out.println("fez xixi");
+        } 
+		else
+		{
+          System.out.println("não fez xixi");
+        }			
+	}
+	
+	public int getIdade(){
+		return idade;
+	}
+	
+	public void setIdade(int idade){
+		this.idade = idade;
+	}
+	
+	public float getComprimento(){
+		return comprimento;
+	}
+	
+	public float setComprimento(float comprimento){
+	    this.comprimento = comprimento;	
+	}
+	
+	public String getNome(){
+		return nome;
+	}
+	
+	public void setNome(String nome){
+		this.nome = nome;
 	}
 	
 	
-	
-	
+
 	
 }
