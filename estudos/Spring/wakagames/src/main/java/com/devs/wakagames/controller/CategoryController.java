1678a714198e5ab143afb008bdadfc3360ebef8e
Carlos Henrique <carlosh.carmo@hotmail.com>
2021-08-02 22:42:05 -0300

    Inserção de mapeamentos no ProductController

--- estudos/Spring/wakagames/src/main/java/com/devs/wakagames/controller/CategoryController.java
+++ estudos/Spring/wakagames/src/main/java/com/devs/wakagames/controller/CategoryController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-@CrossOrigin()
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class CategoryController {
 
 	@Autowired
